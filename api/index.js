3659de98804dfa2d3b33516bda32a8499c0a7912
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-01-01 17:01:39 +0000

    remove express-query-parser package

--- api/index.js
+++ api/index.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const { queryParser } = require("express-query-parser");
 const attachRoutes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  queryParser({
-    parseNull: true,
-    parseUndefined: true,
-    parseBoolean: true,
-    parseNumber: true,
-  }),
-);
 
 attachRoutes(app);
 
--- api/utils.js
+++ api/utils.js
@@ -1,3 +1,9 @@
+/**
+ * Wraps an asynchronous function in a try-catch block and automatically checks for errors.
+ * Reduces redundancy.
+ * @param { function } asyncFunction
+ * @returns { Promise }
+ */
 const catchAsyncErrors = (asyncFunction) => {
   return async (req, res, next) => {
     try {
