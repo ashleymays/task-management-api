30757adfe702974e6ce487c41f28aba3de3ac259
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2024-02-25 16:16:18 -0800

    feat (catch-errors): add jsdoc

--- src/shared/catch-errors.js
+++ src/shared/catch-errors.js
@@ -1,3 +1,10 @@
+/**
+ * Wraps asynchronous errors in a try-catch block
+ * and automatically calls the next function.
+ *
+ * @param {RequestHandler} asyncFn
+ * @returns {Promise<void>}
+ */
 export const catchErrors = (asyncFn) => {
   return async (req, res, next) => {
     try {
